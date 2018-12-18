825af3c423afc909b757f14409763780cf1bffa5
Kimi323 <wangmengya2010@yahoo.co.jp>
2018-12-18 23:45:15 +0900

    finished week5 assignment part 1

--- src/spelling/DictionaryBST.java
+++ src/spelling/DictionaryBST.java
@@ -6,14 +6,15 @@ import java.util.TreeSet;
  * @author UC San Diego Intermediate MOOC team
  *
  */
-public class DictionaryBST implements Dictionary 
-{
-   private TreeSet<String> dict;
+public class DictionaryBST implements Dictionary {
+	private TreeSet<String> dict;
 	
     // TODO: Implement the dictionary interface using a TreeSet.  
  	// You'll need a constructor here
-	
-    
+	public DictionaryBST() {
+		dict = new TreeSet<String>();
+	}
+	    
     /** Add this word to the dictionary.  Convert it to lowercase first
      * for the assignment requirements.
      * @param word The word to add
@@ -21,6 +22,11 @@ public class DictionaryBST implements Dictionary
      * (it wasn't already there). */
     public boolean addWord(String word) {
     	// TODO: Implement this method
+    	word = word.toLowerCase();
+    	if (!dict.contains(word)) {
+    		dict.add(word);
+    		return true;
+    	}
         return false;
     }
 
@@ -29,12 +35,16 @@ public class DictionaryBST implements Dictionary
     public int size()
     {
     	// TODO: Implement this method
-        return 0;
+    	return dict.size();
     }
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
     	//TODO: Implement this method
+    	s = s.toLowerCase();
+    	if (dict.contains(s)) {
+    		return true;
+    	}
         return false;
     }
 
